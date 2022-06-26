b83cc2695f8a029722512b0ac1c7f59f32392023
Pascal Syma <pascal@syma.dev>
2022-06-26 18:30:56 +0200

    Add pinia, persisted state and i18n

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,32 @@
 import { defineNuxtConfig } from 'nuxt'
+import {IntlifyModuleOptions} from "@intlify/nuxt3";
 
+declare module '@nuxt/schema' {
+    interface NuxtConfig {
+        intlify?: IntlifyModuleOptions
+    }
+}
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
     buildModules: [
         '@nuxtjs/tailwindcss',
-        '@nuxtjs/color-mode'
+        '@nuxtjs/color-mode',
+        '@pinia/nuxt',
+        '@intlify/nuxt3'
     ],
     tailwindcss: {
         exposeConfig: true
     },
     colorMode: {
         classSuffix: ''
+    },
+    intlify: {
+        localeDir: 'locales',
+        vueI18n: {
+            locale: 'en',
+            fallbackLocale: 'en',
+            availableLocales: ['en', 'de'],
+        },
     }
 
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
+import {useCounterStore} from "~/store/counter";
+
 const colorMode = useColorMode()
 
-const count = ref(0)
+const counter = useCounterStore()
 </script>
 
 <template>
@@ -13,9 +15,9 @@ const count = ref(0)
         hover:bg-yellow-600
         text-rose-200
         transition duration-300 ease-out"
-        @click.prevent="count++"
-    >{{ count }}</button>
-    <h1 class="text-slate-900 dark:text-white">Color mode: {{ $colorMode.preference }}</h1>
+        @click.prevent="counter.increment()"
+    >{{ counter.count }}</button>
+    <h1 class="text-slate-900 dark:text-white">{{ $t('darkmode.mode', {mode: $t(`darkmode.${$colorMode.preference}`)}) }}</h1>
 
   </div>
 </template>
