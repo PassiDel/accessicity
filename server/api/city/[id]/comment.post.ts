45b94b443bad38362fffc3dee23ac380885e9517
Pascal Syma <pascal@syma.dev>
2022-07-03 01:15:21 +0200

    Implement API add comment
    Signed-off-by: Pascal Syma <pascal@syma.dev>

--- server/api/city/[id]/comment.post.ts
+++ server/api/city/[id]/comment.post.ts
@@ -1,7 +1,21 @@
-import {createError, defineEventHandler, sendError, useQuery} from "h3";
+import {createError, defineEventHandler, sendError, useBody} from "h3";
 import {PrismaClient} from "@prisma/client";
 
 const prisma = new PrismaClient()
+
+type DisabilityRating = {
+    id: number,
+    rating: number
+};
+
+interface AddCommentPayload {
+    title: string,
+    message: string,
+    rating: number,
+    disability?: [
+        DisabilityRating
+    ]
+}
 
 export default defineEventHandler(async (event) => {
     if (!event.context.user)
@@ -9,5 +23,27 @@ export default defineEventHandler(async (event) => {
 
     // TODO: Add feature: add a new comment to a city
 
-    return false
+    const userId = event.context.user.id;
+    const cityId = parseInt(event.context.params.id)
+    // TODO: add input validation
+
+    const {title, message, rating, disability} = await useBody<AddCommentPayload>(event)
+    return await prisma.comment.create({
+        data: {
+            cityId,
+            authorId: userId,
+            title,
+            message,
+            rating,
+            disability: {
+                create: disability.map(d => {
+                    return {
+                        rating: d.rating,
+                        disabilityId: d.id
+                    }
+                })
+            }
+        }
+    })
+
 })
