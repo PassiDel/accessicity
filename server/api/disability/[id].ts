d5964f8894e21461b47f7ac9aa32e4de6804df36
Pascal Syma <pascal@syma.dev>
2022-07-02 23:18:16 +0200

    Implement API disability and ranking based on disability
    Signed-off-by: Pascal Syma <pascal@syma.dev>

--- server/api/disability/[id].ts
+++ server/api/disability/[id].ts
@@ -7,7 +7,47 @@ export default defineEventHandler(async (event) => {
     if (!event.context.user)
         return sendError(event, createError({statusCode: 403, statusMessage: 'Forbidden'}))
 
-    // TODO: Add feature: get all rankings paginated for a disability for every city
+    const amountOfRankings = 1;
+    const disabilityId = parseInt(event.context.params.id)
 
-    return false
+    const {page = "1"} = useQuery(event)
+
+    const skip = typeof page === "string" ? (parseInt(page) - 1) * amountOfRankings : 0
+
+    const rankings = await prisma.ranking.findMany({
+        skip,
+        take: amountOfRankings,
+        where: {
+            disabilityId
+        },
+        orderBy: {
+            value: 'desc'
+        },
+        select: {
+            city: {
+                select: {
+                    id: true,
+                    name: true,
+                    slug: true
+                }
+            },
+            value: true,
+        }
+    })
+
+    const count = await prisma.ranking.count({
+        where: {
+            disabilityId
+        },
+    })
+
+    return {
+        data: rankings,
+        pagination: {
+            offset: skip,
+            limit: amountOfRankings,
+            total: count,
+            lastPage: Math.ceil(count / amountOfRankings)
+        }
+    }
 })
