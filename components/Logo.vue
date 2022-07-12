6b336d5d0e448c1727e11442ebe68d0a528d88cf
Pascal Syma <pascal@syma.dev>
2022-07-13 00:43:11 +0200

    Fix weird width
    Signed-off-by: Pascal Syma <pascal@syma.dev>

--- components/Logo.vue
+++ components/Logo.vue
@@ -5,33 +5,33 @@
 <template>
   <svg class="hover:font-bold" viewBox="0 0 1920 1080" xmlns="http://www.w3.org/2000/svg">
     <rect class="cls-3" height="40" width="40" x="109.5" y="421.5"/>
-    <rect class="cls-2" height="39.97" width="39.97" x="109.5" y="481.47"/>
-    <rect class="cls-2" height="39.97" width="39.97" x="169.46" y="421.51"/>
-    <rect class="cls-2" height="39.97" width="39.97" x="169.46" y="481.47"/>
-    <rect class="cls-2" height="39.97" width="39.97" x="109.5" y="541.43"/>
-    <rect class="cls-2" height="39.97" width="39.97" x="169.46" y="541.43"/>
+    <rect class="cls-2" height="40" width="40" x="109.5" y="481.47"/>
+    <rect class="cls-2" height="40" width="40" x="169.46" y="421.51"/>
+    <rect class="cls-2" height="40" width="40" x="169.46" y="481.47"/>
+    <rect class="cls-2" height="40" width="40" x="109.5" y="541.43"/>
+    <rect class="cls-2" height="40" width="40" x="169.46" y="541.43"/>
     <rect class="cls-3" height="40" width="40" x="269.5" y="358.5"/>
-    <rect class="cls-2" height="39.97" width="39.97" x="269.39" y="418.51"/>
+    <rect class="cls-2" height="40" width="40" x="269.39" y="418.51"/>
     <rect class="cls-3" height="40" width="40" x="329.5" y="358.5"/>
-    <rect class="cls-2" height="39.97" width="39.97" x="329.35" y="418.51"/>
-    <rect class="cls-2" height="39.97" width="39.97" x="269.39" y="478.47"/>
-    <rect class="cls-2" height="39.97" width="39.97" x="329.35" y="478.47"/>
+    <rect class="cls-2" height="40" width="40" x="329.35" y="418.51"/>
+    <rect class="cls-2" height="40" width="40" x="269.39" y="478.47"/>
+    <rect class="cls-2" height="40" width="40" x="329.35" y="478.47"/>
     <rect class="cls-3" height="40" width="40" x="429.5" y="358.5"/>
-    <rect class="cls-2" height="39.97" width="39.97" x="429.28" y="418.51"/>
+    <rect class="cls-2" height="40" width="40" x="429.28" y="418.51"/>
     <rect class="cls-3" height="40" width="40" x="489.5" y="358.5"/>
-    <rect class="cls-2" height="39.97" width="39.97" x="489.24" y="418.51"/>
-    <rect class="cls-2" height="39.97" width="39.97" x="429.28" y="478.47"/>
-    <rect class="cls-2" height="39.97" width="39.97" x="489.24" y="478.47"/>
+    <rect class="cls-2" height="40" width="40" x="489.24" y="418.51"/>
+    <rect class="cls-2" height="40" width="40" x="429.28" y="478.47"/>
+    <rect class="cls-2" height="40" width="40" x="489.24" y="478.47"/>
     <rect class="cls-3" height="40" width="40" x="589.5" y="415.5"/>
-    <rect class="cls-2" height="39.97" width="39.97" x="589.17" y="475.47"/>
-    <rect class="cls-2" height="39.97" width="39.97" x="649.13" y="415.52"/>
+    <rect class="cls-2" height="40" width="40" x="589.17" y="475.47"/>
+    <rect class="cls-2" height="40" width="40" x="649.13" y="415.52"/>
     <rect class="cls-3" height="40" width="40" x="649.5" y="475.5"/>
-    <rect class="cls-2" height="39.97" width="39.97" x="589.17" y="535.43"/>
-    <rect class="cls-2" height="39.97" width="39.97" x="649.13" y="535.43"/>
-    <rect class="cls-2" height="39.97" width="39.97" x="747.07" y="295.6"/>
+    <rect class="cls-2" height="40" width="40" x="589.17" y="535.43"/>
+    <rect class="cls-2" height="40" width="40" x="649.13" y="535.43"/>
+    <rect class="cls-2" height="40" width="40" x="747.07" y="295.6"/>
     <rect class="cls-3" height="40" width="40" x="747.5" y="355.5"/>
     <rect class="cls-3" height="40" width="40" x="807.5" y="295.5"/>
-    <rect class="cls-2" height="39.97" width="39.97" x="807.03" y="355.56"/>
+    <rect class="cls-2" height="40" width="40" x="807.03" y="355.56"/>
     <rect class="cls-3" height="40" width="40" x="747.5" y="415.5"/>
     <rect class="cls-2" height="40" width="40" x="807.5" y="415.5"/>
     <rect class="cls-2" height="40" width="40" x="907.5" y="295.5"/>
