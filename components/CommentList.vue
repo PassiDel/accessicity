6791e9d86ad15bcd0d09e042a194b698a6d7cfde
Pascal Syma <pascal@syma.dev>
2022-07-15 13:50:39 +0200

    Move CommentList to separate component
    Signed-off-by: Pascal Syma <pascal@syma.dev>

---
+++ components/CommentList.vue
@@ -0,0 +1,93 @@
+<script lang="ts" setup>
+
+const {comments, user} = defineProps<{
+  comments: {
+    id: number,
+    createdAt: Date,
+    title: string,
+    message: string,
+    rating: number,
+    city: {
+      slug: string,
+      id: number,
+      name: string
+    },
+    disability: {
+      rating: number,
+      disability: {
+        id: number,
+        name: string,
+        trans_name?: string,
+        slug: string,
+        icon?: string
+      }
+    }[],
+    author?: {
+      id: number,
+      name: string,
+      disabilitys: {
+        verified: boolean,
+        createdAt: Date,
+        disability: {
+          id: number,
+          slug: string,
+          icon: string,
+          trans_name: string,
+          name: string
+        }
+      }[]
+    }
+  }[],
+  user?: {
+    id: number,
+    name: string
+  }
+}>()
+</script>
+
+<template>
+  <div class="w-auto">
+    <div v-for="comment in comments"
+         class="flex space-x-0 md:space-x-10 space-y-5 md:space-y-0 md:flex-row flex-col items-center md:items-start p-5 border-2">
+      <div class="w-52 shrink-0 self-start">
+        <NuxtLink :to="{path: '/explore/' + comment.city.slug}">
+          <h3 class="text-lg font-bold">{{ comment.city.name }}</h3>
+        </NuxtLink>
+        <NuxtLink :to="{path: '/user/' + (comment.author ? comment.author.id : user.id)}">
+          <h4 class="text-md italic">{{ (comment.author ? comment.author.name : user.name) }}</h4>
+        </NuxtLink>
+        <p class="my-2">
+          <DisabilityTag v-for="r in (comment.author ?? user).disabilitys"
+                         :icon="r.disability.icon" :name="r.disability.name"
+                         :trans_name="r.disability.trans_name"
+                         :verified="r.verified"
+                         class="m-1"
+                         style="font-size: .875rem"/>
+        </p>
+        <p>{{ $d(comment.createdAt) }}</p>
+      </div>
+      <div class="w-auto flex-grow">
+        <h4 class="text-md font-bold">{{ comment.title }}</h4>
+        {{ comment.message }}
+      </div>
+      <div class="w-48 shrink-0">
+        <p class="">
+          <DisabilityTag class="ml-1 mr-1" icon="accessibility" name="human" trans_name="disability.wheelchair"
+                         verified="true"/>
+          <Stars :value="comment.rating"/>
+        </p>
+        <p v-for="r in comment.disability" class="place-items-center my-2">
+          <DisabilityTag :icon="r.disability.icon" :name="r.disability.name"
+                         :trans_name="r.disability.trans_name"
+                         :verified="(comment.author ?? user).disabilitys.findIndex(d => d.disability.id === r.disability.id) !== -1"
+                         class="ml-1 mr-1"/>
+          <Stars :value="r.rating"/>
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- pages/user/[[id]].vue
+++ pages/user/[[id]].vue
@@ -6,6 +6,7 @@ import useVuelidate from "@vuelidate/core";
 import {minLength, required} from "~/utils/i18n-validators";
 // noinspection TypeScriptCheckImport
 import {useI18n} from "vue-i18n";
+import CommentList from "~/components/CommentList.vue";
 
 const route = useRoute()
 const auth = useAuthStore()
@@ -71,14 +72,14 @@ const save = async () => {
 
 <template>
   <div v-if="!error" class="xl:px-48 flex flex-col xl:flex-row space-y-10 xl:space-y-0 xl:space-x-10">
-    <div class="w-64 xl:mr-32 mr-0">
+    <div class="w-64 xl:mr-32 mr-0 shrink-0">
       <div v-if="!editMode" class="p-5 border-2 text-center space-y-2">
         <h2 class="text-lg font-bold">{{ data.user.name }}</h2>
         <h3 v-if="isUser" class="text-lg">{{ data.user.email }}</h3>
         <h3 class="text-md font-light">{{ $t('profile.createdAt', {date: $d(data.user.createdAt)}) }}</h3>
         <div>
           <DisabilityTag v-for="d in data.user.disabilitys" :icon="d.disability.icon" :name="d.disability.name"
-                         :trans_name="d.disability.trans_name" :verified="d.verified" class="ml-1 mr-1"/>
+                         :trans_name="d.disability.trans_name" :verified="d.verified" class="m-1"/>
           <button v-if="isUser" class="p-2 px-3 rounded bg-primary hover:bg-primarydark text-light"
                   @click.prevent="openEditMode()">Edit
           </button>
@@ -102,34 +103,7 @@ const save = async () => {
         </div>
       </div>
     </div>
-    <div class="w-auto flex-grow space-y-2.5">
-      <div v-for="comment in data.user.comments" class="flex space-x-10 py-5 border-2">
-        <div class="pl-5 item w-52">
-          <NuxtLink :to="{path: '/explore/' + comment.city.slug}">
-            <h3 class="text-lg font-bold">{{ comment.city.name }}</h3>
-          </NuxtLink>
-          <p>{{ $d(comment.createdAt) }}</p>
-        </div>
-        <div class="item w-auto flex-grow">
-          <h4 class="text-md font-bold">{{ comment.title }}</h4>
-          {{ comment.message }}
-        </div>
-        <div class="item w-52">
-          <p class="">
-            <DisabilityTag class="ml-1 mr-1" icon="accessibility" name="human" trans_name="disability.wheelchair"
-                           verified="true"/>
-            <Stars :value="comment.rating"/>
-          </p>
-          <p v-for="r in comment.disability" class="place-items-center my-2">
-            <DisabilityTag :icon="r.disability.icon" :name="r.disability.name"
-                           :trans_name="r.disability.trans_name"
-                           :verified="data.user.disabilitys.findIndex(d => d.disability.id === r.disability.id) !== -1"
-                           class="ml-1 mr-1"/>
-            <Stars :value="r.rating"/>
-          </p>
-        </div>
-      </div>
-    </div>
+    <CommentList :comments="data.user.comments" :user="data.user" class="flex-grow space-y-2.5"/>
   </div>
   <div v-else>
     <h1 class="dark:text-white">{{ error.data?.statusMessage ?? error.message }}</h1>
