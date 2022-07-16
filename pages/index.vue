d12785c0881eca162983a292ccc945d0f728b8cc
Pascal Syma <pascal@syma.dev>
2022-07-17 01:18:20 +0200

    fix map bug
    Signed-off-by: Pascal Syma <pascal@syma.dev>

--- pages/index.vue
+++ pages/index.vue
@@ -80,16 +80,20 @@ watch(selected, (l, old) => {
       </div>
     </div>
     <div class="w-full h-[33rem] mt-5">
-      <l-map ref="map"
-             @ready="ready = true; map.leafletObject.flyTo(center, zoom)"
-      >
-        <l-tile-layer :attribution="attribution" :url="url"></l-tile-layer>
-        <l-marker v-for="city in result" :lat-lng="[city.city.loc_lat, city.city.loc_lon]"></l-marker>
-        <l-geo-json
-            v-if="selected >= 0"
-            :geojson="{type: 'FeatureCollection', features: [result[selected].city.outline]}"
-        ></l-geo-json>
-      </l-map>
+      <ClientOnly>
+        <l-map ref="map"
+               :center="center"
+               :zoom="zoom"
+               @ready="ready = true; map.leafletObject.flyTo(center, zoom)"
+        >
+          <l-tile-layer :attribution="attribution" :url="url"></l-tile-layer>
+          <l-marker v-for="city in result" :lat-lng="[city.city.loc_lat, city.city.loc_lon]"></l-marker>
+          <l-geo-json
+              v-if="selected >= 0"
+              :geojson="{type: 'FeatureCollection', features: [result[selected].city.outline]}"
+          ></l-geo-json>
+        </l-map>
+      </ClientOnly>
     </div>
     <div v-if="selected > -1">
       <CommentList :city="result[selected].city" :comments="result[selected].city.comments" class="space-y-2.5"/>
