17c4e9a05829dc2443ca023f0279afeb58a87334
Pascal Syma <pascal@syma.dev>
2022-07-18 19:12:45 +0200

    Add textarea input and rework emits
    Signed-off-by: Pascal Syma <pascal@syma.dev>

--- components/InputField.vue
+++ components/InputField.vue
@@ -1,5 +1,4 @@
 <script lang="ts" setup>
-import {ref, watch} from "#imports";
 import {Validation} from "@vuelidate/core";
 
 const {model, type, name, placeholder, v} = defineProps<{
@@ -11,27 +10,34 @@ const {model, type, name, placeholder, v} = defineProps<{
 }>()
 
 const emit = defineEmits(['update:model'])
-
-const _model = ref(model)
-watch(_model, mo => {
-  emit('update:model', mo)
-})
 </script>
 
 <template>
-  <div class="h-[50px] my-8">
+  <div :class="{'h-[50px]': type !== 'textarea'}"
+       class="my-8">
     <div
         :class="{'border-red-500': v?.$errors.length > 0}"
         class="field w-full transform border-b-2 bg-transparent text-lg duration-300 focus-within:border-primary dark:focus-within:border-primarydark"
     >
       <input
-          v-model="_model"
+          v-if="type !== 'textarea'"
+          v-model="model"
+          @input="emit('update:model', model)"
           :name="v?.$path ?? name"
           :type="type"
           class="w-full border-none bg-transparent dark:text-white outline-none placeholder:italic focus:outline-none"
           placeholder=" "
           @blur="v?.$touch()"
       />
+      <textarea v-else
+                v-model="model"
+                :name="v?.$path ?? name"
+                class="w-full border-none bg-transparent dark:text-white outline-none placeholder:italic focus:outline-none max-h-48 min-h-[56px]  resize-y"
+                placeholder=" "
+                @blur="v?.$touch()"
+                @input="emit('update:model', model)"
+      >
+      </textarea>
       <label :for="v?.$path ?? name"
              class="z-[-1] absolute left-0 font-light origin-top-left dark:text-white transition-transform duration-300">{{
           placeholder ?? name ?? ''
@@ -51,7 +57,8 @@ watch(_model, mo => {
 
 /* from https://www.youtube.com/watch?v=yrrw6KdGuxc */
 .field:focus-within label,
-input:not(:placeholder-shown) + label {
+input:not(:placeholder-shown) + label,
+textarea:not(:placeholder-shown) + label {
   @apply -translate-y-6 scale-75
 }
 </style>
