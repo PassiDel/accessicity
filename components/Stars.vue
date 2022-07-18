27716891f53547273ce5f4e3c03b82c55000a0e8
Pascal Syma <pascal@syma.dev>
2022-07-18 19:12:09 +0200

    Make stars clickable (on demand)
    Signed-off-by: Pascal Syma <pascal@syma.dev>

--- components/Stars.vue
+++ components/Stars.vue
@@ -1,10 +1,27 @@
 <script lang="ts" setup>
-const {value} = defineProps<{
+const {value, clickable} = defineProps<{
   value: number,
+  clickable?: boolean
+}>()
+
+const emit = defineEmits<{
+  (e: 'update:value', id: number): void
 }>()
 </script>
 
 <template>
-  <span v-for="i in value" class="material-icons text-button dark:text-buttondark cursor-default">star_rate</span>
-  <span v-for="i in (5 - value)" class="material-icons-outlined text-button dark:text-buttondark cursor-default">star_rate</span>
+  <button v-for="i in 5"
+          v-if="clickable"
+          class="hover:bg-gray-300 dark:hover:bg-gray-600 rounded"
+          @click.prevent="value = value === i ? 0 : i; emit('update:value', value)">
+    <span
+        :class="{'material-icons': value >= i, 'material-icons-outlined': value < i}"
+        class="text-button dark:text-buttondark cursor-default"
+    >star_rate</span>
+  </button>
+  <span v-for="i in 5"
+        v-else
+        :class="{'material-icons': value >= i, 'material-icons-outlined': value < i}"
+        class="text-button dark:text-buttondark cursor-default"
+  >star_rate</span>
 </template>
